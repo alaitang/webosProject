1e31570261e53993aea4bbd2db50f07cd9f604e3
tangalai <tangalai@hotmail.com>
2017-11-21 23:24:12 -0800

    add webosapi files

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+var appRouter = require("./api/routes/routes.js");
+appRouter(app);
+var server = app.listen(4000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
